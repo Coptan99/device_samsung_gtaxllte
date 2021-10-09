eed604de8cc71d5f1eafffa718d3581c8148d9a5
Kyle Harrison <khwebmail@gmail.com>
2021-10-10 00:08:33 +0100

    gtaxl: Move ueventd.samsungexynos7870.rc to /vendor/etc
    In Android 12, the search path for the ueventd vendor config is now /vendor/etc.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -54,7 +54,7 @@ LOCAL_MODULE       := ueventd.samsungexynos7870.rc
 LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := ueventd.samsungexynos7870.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
