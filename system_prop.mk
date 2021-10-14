dfde524bf62c9264656f0a7d24c0a73ad4a8e108
shivatejapeddi <shivatejapeddi@gmail.com>
2021-10-14 10:03:01 +0100

    gtaxl: Set BPF support level
    This allows a legacy device launched with Android (pre-)O and running
    a pre-4.9-P+ kernel to declare itself as not eBPF capable.
    
    Change-Id: If312e06c58535b9005fe5784b460e35438d7dfd9

--- system_prop.mk
+++ system_prop.mk
@@ -77,3 +77,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # System
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.binary_xml=false
+
+# BPF
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.kernel.ebpf.supported=false
