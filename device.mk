266d06caf7be9f139b42c5ae27414ced76b089ca
OmarAlCoptan <omarcoptan9@gmail.com>
2023-03-06 12:31:19 +0000

    gtaxllte: drop audio.a2dp.default
    Signed-off-by: OmarAlCoptan <omarcoptan9@gmail.com>

--- device.mk
+++ device.mk
@@ -209,7 +209,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.bluetooth.audio@2.1-impl \
-    audio.a2dp.default \
     audio.bluetooth.default \
     audio.usb.default \
     audio.r_submix.default \
