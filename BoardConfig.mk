f3a73a7bc34419b6c84f3ebfee059795da06f4da
OmarAlCoptan <omarcoptan9@gmail.com>
2023-03-06 13:02:43 +0000

    gtaxllte: Decommonize dtbhtoolExynos
    Signed-off-by: OmarAlCoptan <omarcoptan9@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,14 +55,10 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # Boot image
-BOARD_CUSTOM_BOOTIMG := true
-BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_IMAGE_NAME := Image
-BOARD_KERNEL_SEPARATED_DT := true
-TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE     := 33554432
